7b16dd3d8d02c86da8568fcae9abb7262d0f6343
Nicolas <84211815+NicolasRay-py@users.noreply.github.com>
2022-06-23 21:14:45 +1000

    Add valid number check
    Add valid number check function to program. Function can check that the number in num range and number is integer.

--- code/guess the number.py
+++ code/guess the number.py
@@ -4,8 +4,19 @@ print('Игра - Угадайка чисел')
 num = randint(1, 100)
 flag = False
 
+
+def is_valid(number):
+    if 101 > number > 0 and isinstance(number, int) == True:
+        return True
+    else:
+        return False
+    
+
 while not flag:
     answer = int(input())
+     if not is_valid(answer):
+        print('Неверный тип данных или число в непраильном диапазоне!')
+        continue
     if answer > num:
         print('Слишком много, попробуйте еще раз')
         continue
