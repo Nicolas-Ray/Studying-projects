16d296a1dc4da6c3f7473198e5dabb8dc9763ed3
Nicolas <84211815+NicolasRay-py@users.noreply.github.com>
2022-06-23 21:41:24 +1000

    Update guess the number.py

--- code/guess the number.py
+++ code/guess the number.py
@@ -1,35 +1,52 @@
 
-from random import *
+from random import randint
 
 print('Игра - Угадайка чисел')
-num = randint(1, 100) 
-flag = False
 
-
-# Valid check function
 
 def is_valid(number):
     if 101 > number > 0 and isinstance(number, int) == True:
         return True
     else:
         return False
-    
-# Guess user number
 
-while not flag:
-    answer = int(input())
-     if not is_valid(answer):
-        print('Неверный тип данных или число в непраильном диапазоне!')
-        continue
-    if answer > num:
-        print('Слишком много, попробуйте еще раз')
-        continue
-    elif answer < num:
-        print('Слишком мало, попробуйте еще раз')
-        continue
-    elif answer == num:
-        print('Вы угадали, поздравляем!')
-        flag = True
 
-print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
-        
+def game(num, flag, counter):
+    while not flag:
+        answer = int(input())
+        if not is_valid(answer):
+            print('Неверный тип данных или число в непраильном диапазоне!')
+            counter += 1
+            continue
+        if answer > num:
+            print('Слишком много, попробуйте еще раз')
+            counter += 1
+            continue
+        elif answer < num:
+            print('Слишком мало, попробуйте еще раз')
+            counter += 1
+            continue
+        elif answer == num:
+            print('Вы угадали, поздравляем!')
+            print()
+            print(f'Вам потребовалось {counter} попыток. ')
+            flag = True
+    print()
+    print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
+
+
+num = randint(1, 100)
+flag = False
+counter = 0
+
+game(num, flag, counter)
+
+restart = input('Хотите начать заново? Да/Нет - ')
+
+if restart == 'Да' or restart == 'да':
+    game(num, flag, counter)
+elif restart == 'Нет' or restart == 'нет':
+    exit(0)
+else:
+    print('Неверное значение, попробуйте еще раз.')
+restart = input('Хотите начать заново? Да/Нет - ')
