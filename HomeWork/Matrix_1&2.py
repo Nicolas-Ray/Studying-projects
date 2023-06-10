051271493964cc70d491a117e93cc2bfde97f3c9
Nikolai Lazovatskii <84211815+Nikolai-Lazovatskii@users.noreply.github.com>
2023-06-11 08:49:45 +1000

    Update and rename Matrix_1.py to Matrix_1&2.py

---
+++ HomeWork/Matrix_1&2.py
@@ -0,0 +1,59 @@
+
+
+# Входные данные
+m = int(input('Enter M:'))
+n = int(input('Enter N:'))
+
+# Функция для создания матрицы
+def make_matrix(m, n):
+    # Пустая матрица
+    matrix = []
+    for i in range(m):
+        # Создания пустой строки
+        column = []
+        for j in range(n):
+            # Добавление элементов в строку
+            column.append(0)
+        # Печать для наглядного вида матрицы
+        print(column)
+        # Добавление строки в патрицу
+        matrix.append(column)
+    return matrix
+
+# Создание матрицы
+outputMatrix = make_matrix(m, n)
+# ЗАДАНИЕ - 1
+
+# Вывод матрицы
+print(outputMatrix)
+
+
+# ЗАДАНИЕ - 2
+
+# Входные данные
+m = int(input('Enter M:'))
+n = int(input('Enter N:'))
+
+# Функция для создания матрицы
+def make_matrix(m, n):
+    # Пустая матрица
+    matrix = []
+    for i in range(m):
+        # Создания пустой строки
+        column = []
+        for j in range(n):
+            # Добавление кастомного элимента
+            el = int(input('Add element:'))
+            # Добавление элементов в строку
+            column.append(el)
+        # Печать для наглядного вида матрицы
+        print(column)
+        # Добавление строки в патрицу
+        matrix.append(column)
+    return matrix
+
+# Создание матрицы
+outputMatrix = make_matrix(m, n)
+
+# Вывод матрицы
+print(outputMatrix)
